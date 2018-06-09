6964c0c29b429aa602eafd7b178663bd3218eec9
TimStolp <tim2stolp@gmail.com>
2018-06-09 23:23:15 +0200

    Cleaned percentage male column

--- Data_2018.py
+++ Data_2018.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import requests
+import math
 
 # Get json data
 file = requests.get('https://www.timeshighereducation.com/sites/default/files/the_data_rankings/world_university_rankings_2018_limit0_369a9045a203e176392b9fb8f8c1cb2a.json').text
@@ -12,34 +13,20 @@ df = pd.DataFrame(data['data']).drop(['apply_link', 'aliases', 'member_level', '
 # Change Female:Male ratio to percentage Male
 df = df.rename({'stats_female_male_ratio':'percentage_male'}, axis=1)
 df['percentage_male'] = df['percentage_male'].apply(lambda val: int(val.split(' : ')[1]) if val is not None else val)
-
+df['stats_number_students'] = df['stats_number_students'].apply(lambda val: int(val.replace(',', '')))
 
 # print(df)
 
+df['male_students'] = ((df.percentage_male / 100) * df.stats_number_students).apply(lambda val: int(val) if not math.isnan(val) else val)
 
-
-########
-# Random test prints
-
-# print(df['percentage_male'])  # Print percentage male column
-# print(df[df['percentage_male'].isnull()][['percentage_male', 'location', 'name']])  # print rows where 'percentage male' has no value
-# print(df.groupby(['location'])['percentage_male'])
-
-df['stats_number_students'] = df['stats_number_students'].apply(lambda val: int(val.replace(',', '')))
-df['male_students'] = (df.percentage_male / 100) * df.stats_number_students
-
-# GET MEAN OF CERTAIN COUNTRY
 sf = (df.groupby(['location'])['male_students'].sum() / df.groupby(['location'])['stats_number_students'].sum() * 100).apply(lambda val: int(val))
 
-mean_df = pd.DataFrame({'location':sf.index, 'mean':sf.values})
+mean_df = pd.DataFrame({'location': sf.index, 'mean': sf.values})
 
-print(df['percentage_male'])
+# print(df[df['location'] == 'Russian Federation'][['location', 'stats_number_students', 'percentage_male', 'male_students']])
 
-# df['percentage_male'].apply(lambda val: mean_df.at(, 'mean')
-
-# print(mean_df[mean_df['Location'] == 'United States']['mean'].values[0])
 for i, row in df.iterrows():
-    if row['percentage_male'].isnull():
+    if math.isnan(row['percentage_male']):
         df['percentage_male'][i] = mean_df[mean_df['location'] == row['location']]['mean'].values[0]
 
 print(df[['location', 'percentage_male']])
